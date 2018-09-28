d60a40cc6db41786e2b5c5fea971b1cee5fa18cf
Poulami Sarkar <poulamirulz@gmail.com>
2018-09-28 05:42:25 +0530

    package.json

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 var url = '/data';
 var messageToDisplay;
 var disp=[0,0];
+var disp2=[0,0];
 
 $(document).ready(function(){
   $('.tabs').tabs();
@@ -21,13 +22,29 @@ $('.btn1').on('click', function() {
   }
 });
 
+//Authors/paper
+$('.btn2').on('click', function() {
+  if($('#se2').is(':checked')){
+    Plotly.purge($('#plot0'));
+    query('select paper_published_year, avg(authors) from (SELECT paper_published_year,paa.paper_ID,COUNT(distinct author_ID) authors from Paper_Author_Affiliations_SE paa, Papers_SE p where paa.paper_ID =p.paper_ID group by (paa.paper_ID)) t1 group by paper_published_year;');
+    query('select paper_published_year, avg(papers) from (SELECT paper_published_year, author_ID,COUNT(distinct paa.paper_ID) papers from Paper_Author_Affiliations_SE paa, Papers_SE p where paa.paper_ID =p.paper_ID group by paper_published_year, author_ID) t2 group by paper_published_year;');    disp2[0] =1;
+    disp2[0] =2;
+  }
+  if($('#am2').is(':checked')){
+    Plotly.purge($('#plot0'));
+    query('SELECT paper_year year,COUNT(distinct paa.paper_ID) papers, COUNT(distinct author_ID) authors from Paper_Author_AM paa, Papers_AM p where paa.paper_ID =p.paper_ID group by (paper_year);');
+    disp2[1] =2;
+  }
+});
+
+
 function onLoad() {
    var response = this.responseText;
    var parsedResponse = JSON.parse(response);
 
    // access your data newly received data here and update your DOM with appendChild(), findElementById(), etc...
    messageToDisplay = parsedResponse;
-   if (disp[0] ==1){
+  if (disp[0] ==1){
     console.log(disp);
     display1(messageToDisplay,0);
   }
@@ -35,7 +52,14 @@ function onLoad() {
     console.log(disp);
     display1(messageToDisplay,1);
    }
-    
+  else if (disp2[0] =0){
+    console.log(disp2);
+    //display2(messageToDisplay,0);
+  }
+  else if (disp2[1] !=0){
+    console.log(disp2);
+    //display2(messageToDisplay,1);
+   }  
 }
 
 function display1(parsedResponse,no){
@@ -64,6 +88,34 @@ function display1(parsedResponse,no){
    Plotly.plot( TESTER, [trace1,trace2] );
    disp[no] =0;
    // append child (with text value o messageToDisplay for instance) here or do some more stuff
+}
+
+function display2(parsedResponse,no){
+  var xval =[];
+  var yval1=[];
+  var yval2=[];
+
+  for(i=0;i<parsedResponse.length;i++){
+    xval.push(parsedResponse[i].year);
+
+    yval1.push(parsedResponse[i].papers);
+    yval2.push(parsedResponse[i].authors);
+  }   
+  TESTER = document.getElementById('plot'+no);
+  var trace1 = {
+   x: xval,
+   y: yval1,
+   mode: 'lines'
+ };
+ 
+ var trace2 = {
+   x: xval,
+   y: yval2,
+   mode: 'lines'
+ };
+  Plotly.plot( TESTER, [trace1,trace2] );
+  disp2[no] =disp2[no]-1;
+  // append child (with text value o messageToDisplay for instance) here or do some more stuff
 }
 
 function onError() {
